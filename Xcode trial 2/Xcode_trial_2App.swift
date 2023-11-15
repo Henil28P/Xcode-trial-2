3ed13cd280eae2a6e4819b70afdd1a0aa82967f4
Henil <henilp467@gmail.com>
2023-11-15 15:02:41 +1100

    Initial Commit

---
+++ Xcode trial 2/Xcode_trial_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_trial_2App.swift
+//  Xcode trial 2
+//
+//  Created by Henil Patel on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_trial_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
