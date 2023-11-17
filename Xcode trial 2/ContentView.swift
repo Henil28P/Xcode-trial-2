b65bf1b1c49ef33f595a5d32d22a4d78a092a0b9
Henil <henilp467@gmail.com>
2023-11-17 13:31:31 +1100

    Input fields

--- Xcode trial 2/ContentView.swift
+++ Xcode trial 2/ContentView.swift
@@ -9,6 +9,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var firstName = ""
+    @State private var password = ""
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +19,11 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Flexy")
         }
+        Form {
+            TextField("First Name", text: $firstName)
+            TextField("Password", text: $password)
+        }
+        .navigationTitle("Login")
         .padding()
     }
 }
